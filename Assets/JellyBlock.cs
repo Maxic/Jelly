64bd9441404a82d48205c0936c0cec2867c40e02
Barend <barend.poot@gmail.com>
2019-04-11 08:09:39 +0200

    setup dict with coords

--- Assets/JellyBlock.cs
+++ Assets/JellyBlock.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class JellyBlock : MonoBehaviour
 {
 	public GameObject smallCube;
+    public int cubeSize;
 
     // Start is called before the first frame update
     void Start()
@@ -13,21 +14,23 @@ public class JellyBlock : MonoBehaviour
         #if UNITY_EDITOR
         UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
         #endif
-        Map<Vector3, GameObject> cubes = new Map<Vector3, GameObject>();
-        List<GameObject> cubes = new List<GameObject>();
 
-        for(float i=0; x < .6f; i += .1f){
-        	for(float j=0; y < .6f; j += .1f){
-        		for(float h=0; z < .6f; h += .1f){
-                    GameObject cube = Instantiate(smallCube, new Vector3(i,j,h), new Quaternion(1,1,1,1)) as GameObject;
-                    cubes[new Vector3(x,y,z)] = cube;
+        var cubes = new Dictionary<Vector3, GameObject>();
+        Vector3 coordinates;
+        GameObject originCube;
+
+        for(float x=0; x < .6f; x += .1f){
+        	for(float y=0; y < .6f; y += .1f){
+        		for(float z=0; z < .6f; z += .1f){
+                    coordinates = new Vector3(x,y,z);
+                    GameObject cube = Instantiate(smallCube, coordinates, new Quaternion(1,1,1,1)) as GameObject;
+                    cubes.Add(coordinates, cube);
 
                     if (cubes.Count > 1){
-                        for(int cubeIndex = 0; cubeIndex < cubes.Count-1; cubeIndex += 3){
-                            FixedJoint fixedJoint = cube.AddComponent<FixedJoint>();
-                            // Connect to neighbours
-                            //fixedJoint.connectedBody = cubes[cubeIndex].GetComponent<Rigidbody>();
-                        }
+                        FixedJoint fixedJoint = cube.AddComponent<FixedJoint>();
+                        cubes.TryGetValue(new Vector3(0,0,0), out originCube);
+                        // remove this and connect to neighbours
+                        fixedJoint.connectedBody = originCube.GetComponent<Rigidbody>();
                     }
         		}
         	}

54b0749e1b1fdcd466a61da387ee271389dd38f6
Barend <barend.poot@gmail.com>
2019-04-11 07:53:48 +0200

    setup map with coords

--- Assets/JellyBlock.cs
+++ Assets/JellyBlock.cs
@@ -13,19 +13,20 @@ public class JellyBlock : MonoBehaviour
         #if UNITY_EDITOR
         UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
         #endif
-
+        Map<Vector3, GameObject> cubes = new Map<Vector3, GameObject>();
         List<GameObject> cubes = new List<GameObject>();
 
-        for(float i=0; i < .6f; i += .1f){
-        	for(float j=0; j < .6f; j += .1f){
-        		for(float h=0; h < .6f; h += .1f){
+        for(float i=0; x < .6f; i += .1f){
+        	for(float j=0; y < .6f; j += .1f){
+        		for(float h=0; z < .6f; h += .1f){
                     GameObject cube = Instantiate(smallCube, new Vector3(i,j,h), new Quaternion(1,1,1,1)) as GameObject;
-                    cubes.Add(cube);
+                    cubes[new Vector3(x,y,z)] = cube;
 
                     if (cubes.Count > 1){
                         for(int cubeIndex = 0; cubeIndex < cubes.Count-1; cubeIndex += 3){
                             FixedJoint fixedJoint = cube.AddComponent<FixedJoint>();
-                            fixedJoint.connectedBody = cubes[cubeIndex].GetComponent<Rigidbody>();
+                            // Connect to neighbours
+                            //fixedJoint.connectedBody = cubes[cubeIndex].GetComponent<Rigidbody>();
                         }
                     }
         		}

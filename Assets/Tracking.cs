3a792a692c1584d999a74515144f41999583c432
Barend Poot <barend.poot@persgroep.net>
2019-04-13 14:35:16 +0200

    fixed compiler errors

--- Assets/Tracking.cs
+++ Assets/Tracking.cs
@@ -24,8 +24,6 @@ public class Tracking : MonoBehaviour
             myTargetLookAtRotation = Quaternion.LookRotation(myTargetLastKnownPosition = transform.position);
 
         }
-
-        if(transforms
     }
         bool SetTarget(GameObject target)
         {

28e57947cd75e98e4f8044759f8ed4f11f8e0f9f
Barend Poot <barend.poot@persgroep.net>
2019-04-13 14:44:03 +0200

    got some more stuff from master

---
+++ Assets/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public float m_Speed;
+    
+
+     void Start()
+    {
+        //Fetch the Rigidbody component you attach from your GameObject
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //Set the speed of the GameObject
+        m_Speed = 30f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
+            m_Rigidbody.velocity = transform.forward * m_Speed;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
+            m_Rigidbody.velocity = -transform.forward * m_Speed;
+        }
+    }
+
+}
